d47ccb4af22be6ca561fef080b715546f7e5eb2b
gray-huntr <lkulecho2001@gmail.com>
2023-12-09 00:29:25 +0300

    App bar added and centered

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-  home: Text('Hey lennox'),
+void main() => runApp( MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: const Text('First app'),
+      centerTitle: true,
+    ),
+  ),
 ));
